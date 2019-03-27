9f463635432209584a3530dc71abc6f4a5d4fabf
Mylinh Pham <mylinhpham@users.noreply.github.com>
2019-03-26 19:31:34 -0700

    Initial Commit

---
+++ TipMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipMe
+//
+//  Created by My Linh Pham on 3/26/19.
+//  Copyright © 2019 My Linh Pham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ TipMeTests/TipMeTests.swift
@@ -0,0 +1,34 @@
+//
+//  TipMeTests.swift
+//  TipMeTests
+//
+//  Created by My Linh Pham on 3/26/19.
+//  Copyright © 2019 My Linh Pham. All rights reserved.
+//
+
+import XCTest
+@testable import TipMe
+
+class TipMeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
